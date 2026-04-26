30964c98ff048a861b2d454fdf0bd97035026d29
abhinavanshu465-rgb <abhinavanshu465@gmail.com>
2026-04-26 21:14:01 +0530

    intro function added in index file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const age =25;
+const age1 =25;
 let name = "anshu";
 const a= 9;
 function f(){
     console.log("Hii");
 }
 const x = 99;
+const pagal = 18;
+function intro(){
+    console.log("welcome");
+}
