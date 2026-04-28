9a679ceffadeadfb094abeb32a91909c9a602e59
abhinavanshu465-rgb <abhinavanshu465@gmail.com>
2026-04-28 12:26:24 +0530

    Revert "intro 8"
    This reverts commit 716e69b4a1f4a10c32ea4ef7d6a3966f9037344f.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const age1 =25;
+const age =25;
 let name = "anshu";
 const a= 9;
 function f(){
@@ -9,21 +9,18 @@ const pagal = 18;
 function intro(){
     console.log("welcome");
 }
-function intro3(){
+function intro2(){
     console.log("welcome2");
 }
-function intro4(){
-    console.log("welcome2");
+function intro5(){
+    console.log("welcome5");
+}
+function intro6(){
+    console.log("welcome6");
 }
 function intro7(){
     console.log("welcome6");
 }
 function intro8(){
-    console.log("welcome6");
-}
-function intro10(){
-    console.log("welcome6");
-}
-function intro11(){
     console.log("welcome6");
 }
